97dd85f1b86c80c8c3440170ba39434e132831df
khounnoukpe <85324916+khounnoukpe@users.noreply.github.com>
2021-07-22 21:20:14 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "nupay-code-sharing-bucket"
-        key = "PipeId"
+        key = "jt.tfstate"
         region = "us-west-2"
         dynamodb_table = "terraform-jenkins-pipeline"
         
